5d291f227dad3eae50b0711c84990301c00f94e3
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-03-29 16:40:46 -0400

    icons

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import { PlayerModel } from '../scripts/model';
+import React from "react";
+import { PlayerModel } from "../scripts/model";
 
 // TODO: (maybe) move player component into PlayerList component to reduce file clutter
-function Player({ player, removePlayer }: { player: PlayerModel, removePlayer: (e: string) => void }) {
-    const warningStyle = {
-        display: player.hasReplays ? 'none' : 'inline'
-    }
+function Player({
+  player,
+  removePlayer,
+}: {
+  player: PlayerModel;
+  removePlayer: (e: string) => void;
+}) {
+  const warningStyle = {
+    display: player.hasReplays ? "none" : "inline",
+  };
 
-    return (
-        <div className='player-container'>
-            <div className='player'>{player.name}<span style={warningStyle} className="material-icons-outlined" title='No replays found for this player'>priority_high</span></div>
-            <button onClick={() => removePlayer(player.name)}>Remove</button>
-        </div>
-    );
+  return (
+    <div className="player-container">
+      <div className="player">
+        {player.name}
+        <span
+          style={warningStyle}
+          className="material-icons-outlined"
+          title="No replays found for this player"
+        >
+          priority_high
+        </span>
+      </div>
+      <button
+        className="material-icons-outlined"
+        onClick={() => removePlayer(player.name)}
+      >
+        remove_circle_outline
+      </button>
+    </div>
+  );
 }
 
 export default Player;
