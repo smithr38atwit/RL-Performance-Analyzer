fa7ac51324454256745b8e1e2b9151d318ae9c9b
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-03-30 15:40:37 -0400

    Initial Dials Implemented

--- frontend/src/components/Stats.tsx
+++ frontend/src/components/Stats.tsx
@@ -1,15 +1,54 @@
 import React from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Stats() {
+    const percentage = 66;
     return (
         <section id="player-stats">
             <div id="main-scores" className="scores">
-                <span id="offense" className="score">Score</span>
-                <span id="defense" className="score">Score</span>
-                <span id="overall" className="score">Score</span>
-                <span className="score-label">Offense</span>
-                <span className="score-label">Defense</span>
-                <span className="score-label">Overall</span>
+                <div >
+                    <CircularProgressbar styles={buildStyles({
+                        // Rotation of path and trail, in number of turns (0-1)
+                        rotation: 0.25,
+                        // Text size
+                        textSize: '16px',
+                        // Colors
+                        pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+                        textColor: '#f88',
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',})}
+                        className="dial-offense" value={percentage} text={`${percentage}%`} />
+                    <p className="score-label">Offense</p>
+                </div>
+                <div>
+                    <CircularProgressbar styles={buildStyles({
+                        // Rotation of path and trail, in number of turns (0-1)
+                        rotation: 0.25,
+                        // Text size
+                        textSize: '16px',
+                        // Colors
+                        pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+                        textColor: '#f88',
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',})}
+                        className="dial-offense" value={percentage} text={`${percentage}%`} />
+                    <p className="score-label">Defense</p>
+                </div>
+                <div>
+                    <CircularProgressbar styles={buildStyles({
+                        // Rotation of path and trail, in number of turns (0-1)
+                        rotation: 0.25,
+                        // Text size
+                        textSize: '16px',
+                        // Colors
+                        pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+                        textColor: '#f88',
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',})}
+                       className="dial-offense" value={percentage} text={`${percentage}%`} />
+                    <p className="score-label">Overall</p>
+                 </div>
             </div>
         </section>
     );
