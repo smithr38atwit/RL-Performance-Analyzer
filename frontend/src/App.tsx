2b8c4bc02ee23d35fe75621791ca17bd19310866
Ryan Smith <smithr38@wit.edu>
2023-04-01 13:41:03 -0400

    oops

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/index.css';
 import PlayerBox from './components/PlayerBox';
-import Scores from './components/Stats';
+import Stats from './components/Stats';
 import 'react-circular-progressbar/dist/styles.css';
 
 
@@ -16,7 +16,7 @@ function App() {
           <a href="#player-stats">Player Statistics</a>
           <a href="#team-stats">Team Statistics</a>
         </nav>
-        <Scores />
+        <Stats />
       </main>
     </>
   );
