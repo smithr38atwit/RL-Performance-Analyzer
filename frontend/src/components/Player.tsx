b937b9da585ded3826b1a9e4d082ed912550f510
Ryan Smith <smithr38@wit.edu>
2023-03-31 22:06:39 -0400

    reformat

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { PlayerModel } from "../scripts/model";
 
-// TODO: (maybe) move player component into PlayerList component to reduce file clutter
-function Player({
-  player,
-  removePlayer,
-}: {
-  player: PlayerModel;
-  removePlayer: (e: string) => void;
-}) {
+function Player({ player, removePlayer, }: { player: PlayerModel; removePlayer: (e: string) => void; }) {
   const warningStyle = {
     display: player.hasReplays ? "none" : "inline",
   };
@@ -17,20 +10,9 @@ function Player({
     <div className="player-container">
       <div className="player">
         {player.name}
-        <span
-          style={warningStyle}
-          className="material-icons-outlined"
-          title="No replays found for this player"
-        >
-          priority_high
-        </span>
+        <span style={warningStyle} className="material-icons-outlined" title="No replays found for this player">priority_high</span>
       </div>
-      <button
-        className="material-icons-outlined"
-        onClick={() => removePlayer(player.name)}
-      >
-        remove_circle_outline
-      </button>
+      <button className="material-icons-outlined" onClick={() => removePlayer(player.name)} >remove_circle_outline</button>
     </div>
   );
 }
