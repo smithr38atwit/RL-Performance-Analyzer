47217ac5550338828fb706bcf155b1e7516e1105
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-04-04 11:52:08 -0400

    cleanup

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from uuid import UUID 
+from uuid import UUID
 import requests
 import os
 from dotenv import load_dotenv
