03c279004f28b816bba94ebc6d604718afd1e4db
chinr1atwit <chinr1@wit.edu>
2023-04-06 10:29:10 -0400

    did changes to dials

--- frontend/src/components/Stats.tsx
+++ frontend/src/components/Stats.tsx
@@ -3,32 +3,49 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function StatsDial(percentage: number) {
+    let r = 0, g = 0, b = 0;
+    //if score is above 80, graph is green
+    if(percentage >= 80){
+        g = 255;
+    }
+    //if score is between 80 and 60, graph is yellow
+    else if(percentage < 80 && percentage >= 60){
+        r = 255;
+        g = 255;
+    }
+    //if score is below 60, graph is red
+    else {
+        r = 210;
+        g = 4;
+        b = 45;
+    }
     return (
         <CircularProgressbar
             value={percentage}
             text={`${percentage}`}
-            strokeWidth={25}
+            strokeWidth={23}
+            counterClockwise={true}
+            background={true}
             styles={{
                 // Customize the root svg element
                 root: {},
                 // Customize the path, i.e. the "completed progress"
                 path: {
                     // Path color
-                    stroke: `rgba(164, 44, 214, ${percentage / 100})`,
+                    stroke: `rgba(${r}, ${g}, ${b}, 100)`,
                     // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: 'round',
+                    strokeLinecap: 'butt',
                     // Customize transition animation
                     transition: 'stroke-dashoffset 0.5s ease 0s',
                     // Rotate the path
-                    transform: 'rotate(0.25turn)',
                     transformOrigin: 'center center',
+                    transitionDuration: '1',
                 },
                 // Customize the circle behind the path, i.e. the "total progress"
                 trail: {
                     // Trail color
-                    stroke: '#C179B9',
+                    stroke: '#D3D3D3',
                     // Rotate the trail
-                    transform: 'rotate(0.25turn)',
                     transformOrigin: 'center center',
                 },
                 // Customize the text
@@ -36,13 +53,13 @@ function StatsDial(percentage: number) {
                     // Text color
                     fill: '#2F242C',
                     // Text size
-                    fontSize: '16px',
+                    fontSize: '23px',
                     fontWeight: 'bold',
                     fontFamily: 'sans-serif',
                 },
                 // Customize background - only used when the `background` prop is true
                 background: {
-                    fill: '#3e98c7',
+                    fill: '#A9A9A9',
                 },
             }}
         />
@@ -50,20 +67,20 @@ function StatsDial(percentage: number) {
 }
 
 function Stats({ stats }: { stats: any[] }) {
-    const percentage = 66;
+    const percentage1 = 50, percentage2 = 90, percentage3 = 70;
     return (
         <section id="player-stats">
             <div id="main-scores" className="scores">
                 <div >
-                    {StatsDial(percentage)}
+                    {StatsDial(percentage1)}
                     <p className="score-label">Offense</p>
                 </div>
                 <div>
-                    {StatsDial(percentage)}
+                    {StatsDial(percentage2)}
                     <p className="score-label">Defense</p>
                 </div>
                 <div>
-                    {StatsDial(percentage)}
+                    {StatsDial(percentage3)}
                     <p className="score-label">Overall</p>
                 </div>
             </div>
